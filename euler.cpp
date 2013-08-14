acb21bd553470e3dc9160e6772993819208c9678
Diplofocus <psaulic@gmail.com>
2013-08-15 01:09:48 +0200

    n-body report and to-dos

--- euler.cpp
+++ euler.cpp
@@ -7,27 +7,34 @@
 
 using namespace std;
 
-const int max_time = 1000;
 const int elements = 2;
 const double G = 6.67E-11;
 const double h = 0.1;
-const double dt = 1.0;
-const double tmax = 1000;
+const double dt = 1000.0;
+const double tmax = 30E6;
+
  class pState
 {
         public:
         double x,y;
+	double x1,x2,x3,x4;
+	double y1,y2,y3,y4;
         double vx,vy;
+	double vx1,vx2,vx3,vx4;
+	double vy1,vy2,vy3,vy4;
         double ax,ay,a;
+	double ax1,ax2,ax3,ax4;
+	double ay1,ay2,ay3,ay4;
+	double a1,a2,a3,a4;
         double m;
-
-/*	pState()
+/*
+	pState()
 	{
-        x = rand() % 100 - 50;
-        y = rand() % 100 - 50;
-        vx = rand() % 10 - 5 ;
-        vy = rand() % 10 - 5;
-	m = rand() % 30 + 5;
+        x = rand() % 150000000;
+        y = rand() % 150000000;
+        vx = rand() % 15;
+        vy = rand() % 15;
+	m = rand() % 700000000000000000000000;
 	}
 */
 };
@@ -37,14 +44,21 @@ double sqr(float a)
     return a * a;
 }
 
+double cube(float a)
+{
+	return a * a * a;
+}
+
 double dist(pState A, pState B)
 {
     return sqrt(sqr(B.x - A.x) + sqr(B.y - A.y));
 }
 
+double rdist()
+
 double FGrav(pState A, pState B)
 {
-	double F = - G * A.m * B.m / sqr(dist(A, B));
+	double F = - G * A.m * B.m / cube(dist(A, B));
 	return F;
 }
 
@@ -70,7 +84,7 @@ int main()
 	particle[1].y = 0;
 	particle[1].vx = 0;
 	particle[1].vy = 30E3;
-	particle[1].m = 6E24;
+	particle[1].m = 8E24;
 
 
 	double perc = 0;
@@ -95,12 +109,19 @@ int main()
 			continue;
 			}
 	//Acceleration
-		         particle[j].a = FGrav(particle[i], particle[j]) / particle[j].m;
-		   	 particle[j].ax = particle[j].a * (particle[j].x - particle[i].x) / dist(particle[j], particle[i]);
-		   	 particle[j].ay = particle[j].a * (particle[j].y - particle[i].y) / dist(particle[j], particle[i]);
+		         particle[j].a1 = FGrav(particle[j], particle[i]) / particle[j].m;
+		   	 particle[j].ax1 = particle[j].a * (particle[j].x - particle[i].x);
+		   	 particle[j].ay1 = particle[j].a * (particle[j].y - particle[i].y);
 
+			particle[j].vx1 = particle[j].vx;
+			particle[j].vy1 = particle[j].vy;
+
+			particle[j].x1 = particle[j].x;
+			particle[j].y1 = particle[j].y;
+
+
+			particle[j].a2 = -G * particle[i].m * particle[j].x2 / (cube(dist(particle[j], particle[i])));
 	        }
-	      	 simout << "#Telo#" << i << endl << particle[i].x << ", " << particle[i].y << ",  " << particle[i].vx << ", " << particle[i].vy << ", " << particle[i].ax << ", " << particle[i].ay << ", " << particle[i].a << endl;
 
 	    }
 
@@ -112,9 +133,11 @@ int main()
 			particle[j].vy += particle[j].ay * dt;
 			particle[j].x += particle[j].vx * dt;
 			particle[j].y += particle[j].vy * dt;
+			 simout << "#Telo#" << i << endl << particle[i].x << ", " << particle[i].y << ",  " << endl;
 			}
 		}
 
 	}
 	simout.close();
+return 0;
 }
